2b5e48af72953d687eef3b4e89c1ecc105555363
Sam Pepler <sam.pepler@stfc.ac.uk>
2025-09-19 15:24:06 +0100

    record option for ls

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='fbi-core',
-    version='1.3.1',
+    version='1.3.2',
     description='File Based Index  (FBI) core tools',
     long_description=long_description,
 
