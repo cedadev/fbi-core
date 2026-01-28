81aa94d91f6cd8d4b233493cb0af5abc13f91e34
William Tucker <william.tucker@stfc.ac.uk>
2026-01-28 16:32:38 +0000

    Incrementing version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='fbi-core',
-    version='1.3.2',
+    version='1.3.3',
     description='File Based Index  (FBI) core tools',
     long_description=long_description,
 
